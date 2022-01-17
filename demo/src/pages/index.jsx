7958111e6d50b07e4c25f56e852a24b9021eb673
张玉 <zyjavascript@163.com>
2022-01-17 13:49:58 +0800

    添加gitHooks

--- demo/src/pages/index.jsx
+++ demo/src/pages/index.jsx
@@ -14,17 +14,6 @@ export default function IndexPage() {
       <h1 className={styles.title}>
         <p>全局注入参数: {JSON.stringify(globalState)}</p>
         <p>props传递参数:  {testProp1}</p>
-        <button onClick={() => setModalVisible(true)}>全局组件启动</button>
-        <CustomModal
-          title="全局组件"
-          visible={modalVisible}
-          okText={'确定'}
-          onOk={() => setModalVisible(false)}
-          onCancel={() => setModalVisible(false)}
-          width={400}
-        >
-          demo
-        </CustomModal>
       </h1>
     </div>
   );
