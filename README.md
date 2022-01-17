8b8b972c5eea327be5ab5d5357954ac37a5637d6
张玉 <zyjavascript@163.com>
2022-01-17 11:02:48 +0800

    修改md

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
-> 使用手册
-### 说明文档
+## 说明文档
+#### 框架使用说明
   https://umijs.org/zh-CN/plugins/plugin-qiankun
+#### 响应式布局说明
+  https://github.com/evrone/postcss-px-to-viewport
+  在.umi.js中修改 viewportWidth（设计图多宽就设置多大）
+
 ### 新建子应用
   1. 复制粘贴demo并修改名称为A项目
   2. A项目修改成固定端口号  .env文件中修改port值
   3. 在根文件下package.json中添加启动命令
-    
+
 > 插件引入
   https://umijs.org/zh-CN/plugins/plugin-qiankun    qiankun umi插件
   npm-run-all https://www.npmjs.com/package/npm-run-all 可以并行或顺序地运行多个npm脚本
   
-```
-> 
-1. 配置一键启动所有 程序
+> 1. 配置一键启动所有 程序
   下载 npm-run-all package.json添加 
 ```
   "scripts": {
--- demo/.umirc.js
+++ demo/.umirc.js
@@ -19,25 +19,25 @@ export default defineConfig({
   dva: {
     immer: true,
   },
-  // extraPostCSSPlugins: [
-  //   // https://github.com/evrone/postcss-px-to-viewport
-  //   require('postcss-px-to-viewport')({
-  //     unitToConvert: 'px',
-  //     viewportWidth: 1728,
-  //     unitPrecision: 3,
-  //     propList: ['*'],
-  //     viewportUnit: 'vw',
-  //     fontViewportUnit: 'vw',
-  //     selectorBlackList: ['.ignore-', '.hairlines', 'am-', 'px-', '.css'],
-  //     minPixelValue: 1,
-  //     mediaQuery: true,
-  //     replace: true,
-  //     landscape: false,
-  //     landscapeUnit: 'vw',
-  //     landscapeWidth: 1728,
-  //     exclude: [/\/Stores\/.*.less/, /global.css/, /node_modules/],
-  //   }),
-  // ],
+  extraPostCSSPlugins: [
+    // https://github.com/evrone/postcss-px-to-viewport
+    require('postcss-px-to-viewport')({
+      unitToConvert: 'px',
+      viewportWidth: 1728,
+      unitPrecision: 3,
+      propList: ['*'],
+      viewportUnit: 'vw',
+      fontViewportUnit: 'vw',
+      selectorBlackList: ['.ignore-', '.hairlines', 'am-', 'px-', '.css'],
+      minPixelValue: 1,
+      mediaQuery: true,
+      replace: true,
+      landscape: false,
+      landscapeUnit: 'vw',
+      landscapeWidth: 1728,
+      exclude: [/\/Stores\/.*.less/, /global.css/, /node_modules/],
+    }),
+  ],
   // mfsu: {},
   fastRefresh: {},
   // https://github.com/umijs/umi-webpack-bundle-analyzer#options-for-plugin
